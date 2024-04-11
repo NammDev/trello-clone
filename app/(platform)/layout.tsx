da16c3723fe2f9eba3b4f750dd2550c1d2209c1c
Nam <namdeveloper.ca@gmail.com>
2024-04-11 22:11:51 +0700

    authentication

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,21 @@
+import type { PropsWithChildren } from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const PlatformLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: '/logo.svg',
+        },
+        variables: {
+          colorPrimary: '#171717',
+        },
+      }}
+    >
+      {children}
+    </ClerkProvider>
+  )
+}
+
+export default PlatformLayout
