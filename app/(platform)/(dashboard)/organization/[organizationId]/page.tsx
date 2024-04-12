fafc58d5f5b0446ba29bb036cbd98597366cb4b3
Nam <namdeveloper.ca@gmail.com>
2024-04-12 13:32:54 +0700

    match params with org id

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
 import { OrganizationSwitcher, auth } from '@clerk/nextjs'
 
-const OrganizationIdPage = async () => {
+const OrganizationIdPage = async ({
+  params,
+}: {
+  params: {
+    organizationId: string
+  }
+}) => {
   const { userId, orgId } = auth()
 
-  if (!orgId) return false
+  if (orgId !== params.organizationId) return <div>Not Found</div>
 
   return <div className='w-full mb-20'>OrganizationIdPage: {orgId}</div>
 }
