279baf33f686f2f63cb8c683fd860407de8a1805
Nam <namdeveloper.ca@gmail.com>
2024-04-12 15:07:05 +0700

    layout

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from './_components/navbar'
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='h-full'>
+    <div className='relative flex min-h-screen flex-col'>
       <Navbar />
       {children}
     </div>
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -4,12 +4,14 @@ import { Sidebar } from '../_components/sidebar'
 
 const OrganizationLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className='pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>
-      <div className='flex gap-x-7'>
-        <div className='w-64 shrink-0 hidden md:block'>
-          <Sidebar />
+    <main className='flex-1'>
+      <div className='pt-12 md:pt-16 px-4 max-w-6xl 2xl:max-w-[1400px] mx-auto'>
+        <div className='flex gap-x-7'>
+          <div className='w-64 shrink-0 hidden md:block'>
+            <Sidebar />
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </main>
   )
